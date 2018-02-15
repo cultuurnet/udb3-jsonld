e613137096287a3399a565351f27fa78e79ff61f
Bert Ramakers <bertramakers@me.com>
2018-02-15 15:22:08 +0100

    Add value objects for title and description

---
+++ src/ValueObject/Text/Description.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace CultuurNet\UDB3\Model\ValueObject\Text;
+
+use TwoDotsTwice\ValueObject\String\Behaviour\IsNotEmpty;
+use TwoDotsTwice\ValueObject\String\Behaviour\IsString;
+use TwoDotsTwice\ValueObject\String\Behaviour\Trims;
+
+/**
+ * @todo Trim as well?
+ */
+class Description
+{
+    use IsString;
+    use IsNotEmpty;
+
+    /**
+     * @param string $value
+     */
+    public function __construct($value)
+    {
+        $this->guardNotEmpty($value);
+        $this->setValue($value);
+    }
+}
