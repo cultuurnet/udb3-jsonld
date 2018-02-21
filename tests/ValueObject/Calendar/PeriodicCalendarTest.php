f28e9938a6b62002ac144b60badf6b4c1e79288a
Bert Ramakers <bertramakers@me.com>
2018-02-21 16:52:09 +0100

    III-2490: Improve test for periodic calendar

--- tests/ValueObject/Calendar/PeriodicCalendarTest.php
+++ tests/ValueObject/Calendar/PeriodicCalendarTest.php
@@ -2,7 +2,13 @@
 
 namespace CultuurNet\UDB3\Model\ValueObject\Calendar;
 
+use CultuurNet\UDB3\Model\ValueObject\Calendar\OpeningHours\Day;
+use CultuurNet\UDB3\Model\ValueObject\Calendar\OpeningHours\Days;
+use CultuurNet\UDB3\Model\ValueObject\Calendar\OpeningHours\Hour;
+use CultuurNet\UDB3\Model\ValueObject\Calendar\OpeningHours\Minute;
+use CultuurNet\UDB3\Model\ValueObject\Calendar\OpeningHours\OpeningHour;
 use CultuurNet\UDB3\Model\ValueObject\Calendar\OpeningHours\OpeningHours;
+use CultuurNet\UDB3\Model\ValueObject\Calendar\OpeningHours\Time;
 use PHPUnit\Framework\TestCase;
 
 class PeriodicCalendarTest extends TestCase
@@ -56,7 +62,38 @@ class PeriodicCalendarTest extends TestCase
     {
         $startDate = \DateTimeImmutable::createFromFormat('d/m/Y', '10/12/2018');
         $endDate = \DateTimeImmutable::createFromFormat('d/m/Y', '18/12/2018');
-        $openingHours = new OpeningHours();
+
+        $days = new Days(
+            Day::monday(),
+            Day::tuesday(),
+            Day::wednesday()
+        );
+
+        $openingHours = new OpeningHours(
+            new OpeningHour(
+                $days,
+                new Time(
+                    new Hour(9),
+                    new Minute(0)
+                ),
+                $closingTime = new Time(
+                    new Hour(12),
+                    new Minute(0)
+                )
+            ),
+            new OpeningHour(
+                $days,
+                new Time(
+                    new Hour(13),
+                    new Minute(0)
+                ),
+                $closingTime = new Time(
+                    new Hour(17),
+                    new Minute(0)
+                )
+            )
+        );
+
         $calendar = new PeriodicCalendar($startDate, $endDate, $openingHours);
 
         $this->assertEquals($openingHours, $calendar->getOpeningHours());
