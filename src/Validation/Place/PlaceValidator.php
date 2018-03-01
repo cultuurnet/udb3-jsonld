62537d4557c32e738cf877511b778352d85b9394
Bert Ramakers <bertramakers@me.com>
2018-03-01 16:46:46 +0100

    III-2523: Add comment why we don’t validate the place geocoordinates

--- src/Validation/Place/PlaceValidator.php
+++ src/Validation/Place/PlaceValidator.php
@@ -15,6 +15,13 @@ class PlaceValidator extends OfferValidator
 {
     public function __construct()
     {
+        // We don't validate the coordinates because they're optional and are
+        // always based on the address. So when validating incoming json-ld on
+        // UDB3 we don't care about the coordinates, and when validating json-ld
+        // on other systems to deserialize it to the models there's not much we
+        // can do if the coordinates are invalid. The deserializer should just
+        // try to convert them to the correct models, and if it fails it should
+        // just set them to null.
         $rules = [
             new Key('address', new TranslatedAddressValidator(), true),
         ];
