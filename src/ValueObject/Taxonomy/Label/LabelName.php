d81baa1e5917d155c7277804794f5abe9feff5c4
Luc Wollants <luc.wollants@apptite.be>
2018-03-06 15:33:12 +0100

    III-2490 LabelName has some extra constraints (no semicolon, mim 2 chars, max 255 chars).

--- src/ValueObject/Taxonomy/Label/LabelName.php
+++ src/ValueObject/Taxonomy/Label/LabelName.php
@@ -2,20 +2,40 @@
 
 namespace CultuurNet\UDB3\Model\ValueObject\Taxonomy\Label;
 
-use TwoDotsTwice\ValueObject\String\Behaviour\IsNotEmpty;
 use TwoDotsTwice\ValueObject\String\Behaviour\IsString;
+use TwoDotsTwice\ValueObject\String\Behaviour\Trims;
 
 class LabelName
 {
     use IsString;
-    use IsNotEmpty;
+    use Trims;
 
     /**
      * @param string $value
      */
     public function __construct($value)
     {
-        $this->guardNotEmpty($value);
+        $value = $this->trim($value);
+
+        if (false !== strpos($value, ';')) {
+            throw new \InvalidArgumentException(
+                "Label '$value' should not contain semicolons."
+            );
+        }
+
+        $length = mb_strlen($value);
+        if ($length < 2) {
+            throw new \InvalidArgumentException(
+                "Label '$value' should not be shorter than 2 chars."
+            );
+        }
+
+        if ($length > 255) {
+            throw new \InvalidArgumentException(
+                "Label '$value' should not be longer than 255 chars."
+            );
+        }
+
         $this->setValue($value);
     }
 }
