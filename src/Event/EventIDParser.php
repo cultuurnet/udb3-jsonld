edbad1ba673a2d6bbf83c40d33c2b1015c058f6d
Bert Ramakers <bertramakers@me.com>
2018-03-07 11:48:15 +0100

    III-2535: Add RegexUUIDParser base class

--- src/Event/EventIDParser.php
+++ src/Event/EventIDParser.php
@@ -2,30 +2,16 @@
 
 namespace CultuurNet\UDB3\Model\Event;
 
-use CultuurNet\UDB3\Model\ValueObject\Identity\UUID;
-use CultuurNet\UDB3\Model\ValueObject\Identity\UUIDParser;
-use CultuurNet\UDB3\Model\ValueObject\Web\Url;
+use CultuurNet\UDB3\Model\ValueObject\Identity\RegexUUIDParser;
 
-class EventIDParser implements UUIDParser
+class EventIDParser extends RegexUUIDParser
 {
     // @codingStandardsIgnoreStart
     const REGEX = '/\\/event[s]?\\/([0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12})[\\/]?/';
     // @codingStandardsIgnoreEnd
 
-    /**
-     * @inheritdoc
-     */
-    public function fromUrl(Url $url)
+    public function __construct()
     {
-        $url = $url->toString();
-
-        $matches = [];
-        preg_match(self::REGEX, $url, $matches);
-
-        if (count($matches) > 1) {
-            return new UUID($matches[1]);
-        } else {
-            throw new \InvalidArgumentException('No EventID found in given Url.');
-        }
+        parent::__construct(self::REGEX, 'EventID');
     }
 }
