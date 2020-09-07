49cf6589e2900a68bf002ef7a00bbbf2487bb5cc
Bert Ramakers <bertramakers@me.com>
2020-09-07 10:14:13 +0200

    Internalise src and tests from 2dotstwice/value-objects

---
+++ tests/ValueObject/String/Behaviour/MockUUID.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace CultuurNet\UDB3\Model\ValueObject\String\Behaviour;
+
+class MockUUID
+{
+    use IsString;
+    use MatchesUUIDPattern;
+
+    public function __construct($value)
+    {
+        $this->guardString($value);
+        $this->guardUUIDPattern($value);
+        $this->setValue($value);
+    }
+}
