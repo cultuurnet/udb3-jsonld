dc54a1119c9d0ef11c94239ddbed9a107e8d3bf2
Luc Wollants <lucwollants@gmail.com>
2021-01-05 15:08:03 +0100

    Add trait to derive Status from sub events.

---
+++ src/ValueObject/Calendar/DeriveFromSubEvents.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Model\ValueObject\Calendar;
+
+trait DeriveFromSubEvents
+{
+    public function statusFromSubEvents(SubEvents $subEvents): Status
+    {
+        return new Status($this->statusTypeFromSubEvents($subEvents));
+    }
+
+    public function statusTypeFromSubEvents(SubEvents $subEvents): StatusType
+    {
+        $statusTypeCounts = [];
+        $statusTypeCounts[StatusType::Available()->toString()] = 0;
+        $statusTypeCounts[StatusType::TemporarilyUnavailable()->toString()] = 0;
+        $statusTypeCounts[StatusType::Unavailable()->toString()] = 0;
+
+        /** @var SubEvent $subEvent */
+        foreach ($subEvents as $subEvent) {
+            ++$statusTypeCounts[$subEvent->getStatus()->getType()->toString()];
+        }
+
+        if ($statusTypeCounts[StatusType::Available()->toString()] > 0) {
+            return StatusType::Available();
+        }
+
+        if ($statusTypeCounts[StatusType::TemporarilyUnavailable()->toString()] > 0) {
+            return StatusType::TemporarilyUnavailable();
+        }
+
+        if ($statusTypeCounts[StatusType::Unavailable()->toString()] > 0) {
+            return StatusType::Unavailable();
+        }
+
+        // This is added to make sure a return statement is present.
+        // But this line can never be reached because of the required StatusType for a SubEvent.
+        return StatusType::Available();
+    }
+}
